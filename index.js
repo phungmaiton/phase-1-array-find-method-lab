e1a53ce5e0932c92264852c50c594ffaa5ab2415
Mai <garcia.ton94@gmail.com>
2023-05-02 17:29:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records) {
+    function checkWin (record) {
+        if (record.result === 'W') {
+            return record.year;
+        }
+    }
+    const results = records.find(checkWin);
+    return results ? results.year : undefined;
+};
+
